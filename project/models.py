52931b27b106819c93fc2269d72390ee90587a3f
AmmirMahdi <a.m.rashvand@gmail.com>
2020-12-26 10:27:30 +0330

    user model added

--- project/models.py
+++ project/models.py
@@ -1,5 +1,8 @@
 from project import db  
 from datetime import datetime
+
+
+
 class Todo(db.Model):
     __tablename__='todo'
     
@@ -8,3 +11,17 @@ class Todo(db.Model):
     date         = db.Column(db.Date, default=datetime.utcnow())
     description  = db.Column(db.Text)
     complete     = db.Column(db.Boolean)
+    users        = db.relationship('User', backref='todo')
+
+    def __repr__(self):
+        return f"Todo {self.title}"
+
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    todo     = db.Column(db.Integer, db.ForeignKey('todo.id'))
+    
+    def __repr__(self):
+        return f"User {self.username}"
